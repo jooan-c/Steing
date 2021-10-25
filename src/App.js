113220d015a962687a6b64e83570391b341cc6d7
Joo An Choi <jcho274@gmail.com>
2021-10-25 15:26:01 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
-
+import React ,{useState}from 'react'
 function App() {
+
+const [data,setData]=useState(null)
+const [arr,setArr]=useState(null)
+
+  function getData(val)
+  {
+    let arr = [];
+    setData(val.target.value)
+    for (let i = 0; i < val.targe.value; i++) {
+      arr.push("h ")
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="number" onChange={getData} />
+    {data}
+    <div>{arr}</div>
     </div>
   );
 }
